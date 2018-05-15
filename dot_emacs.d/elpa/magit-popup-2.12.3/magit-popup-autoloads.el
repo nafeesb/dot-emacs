6f904bf6828d538b5137853dc3612a63fedfefa3
Nafees Bin Zafar <nbinzafar@ladwu499-l.magicleap.ds>
2018-05-14 18:09:16 -0700

    Updates

---
+++ dot_emacs.d/elpa/magit-popup-2.12.3/magit-popup-autoloads.el
@@ -0,0 +1,16 @@
+;;; magit-popup-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("magit-popup-pkg.el" "magit-popup.el")
+;;;;;;  (23290 12920 297068 237000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; magit-popup-autoloads.el ends here
